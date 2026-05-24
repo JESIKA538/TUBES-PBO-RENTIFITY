e63260a6c2ec82e6547ca13ea86302b69029e8ee
JESIKA538 <rahmaarifinjesikametania@gmai.com>
2026-05-24 22:26:03 +0700

    new

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-    const API_BASE_URL = window.location.hostname === 'localhost'
+ const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://127.0.0.1:8080/api'
-    : 'https://tubes-pbo-rentifity-production.up.railway.app/api';
+    : 'https://rentify-backend-production.up.railway.app/api';
 
 /**
  * Generic API Fetch wrapper that injects headers and bearer tokens,
