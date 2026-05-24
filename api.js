e656d6359b99014f7090032d28bec107ca60a438
JESIKA538 <rahmaarifinjesikametania@gmai.com>
2026-05-24 22:32:23 +0700

    lagi

--- api.js
+++ api.js
@@ -1,5 +1,4 @@
 const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
- const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://127.0.0.1:8080/api'
     : 'https://rentify-backend-production.up.railway.app/api';
 
